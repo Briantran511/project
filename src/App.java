009a11a0aac7a422cd8b2c7a83a9b1f30fc12c65
Briantran511 <trong-thuc.tran@students.mq.edu.au>
2023-11-20 00:38:34 +1100

    Add Login Function

--- src/App.java
+++ src/App.java
@@ -1,20 +1,23 @@
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class App extends JFrame{
     public App(){
         super("App");
-        // Create JPanel
-        // Create JLabel
-        // Create JButton
 
-        
+        // Create JPanel
         JPanel jp = new JPanel();
+
+        //Create JLabel
         JLabel userLabel = new JLabel("Username");
         jp.add(userLabel);
         JTextField userText = new JTextField(10);
@@ -23,14 +26,32 @@ public class App extends JFrame{
         jp.add(passLabel);
         JTextField passText = new JTextField(10);
         jp.add(passText);
+
+        // Create Login button
+        JButton login = new JButton("Login");
+        login.setBounds(50,100,95,30);
+        jp.add(login);
+        login.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               String username = userText.getText();
+               String password = passText.getText();
+               if(username.equals("Admin")&& password.equals("admin")){
+                JOptionPane.showMessageDialog(login, "Login Successful");
+               } else{
+                JOptionPane.showMessageDialog(login, "Invalid Username or password");
+               }
+            }
+            
+        });
         add(jp);
-        setLayout(new FlowLayout(FlowLayout.LEFT));
-        setSize(400,300);
+        jp.setLayout(new FlowLayout(FlowLayout.CENTER));
+        setSize(400,400);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     public static void main(String[] args) {
         new App();
-
     }
 }
